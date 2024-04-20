3b6ace4b4818c82cb3e6bd7cb0d13bbd88339ac1
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-04-20 21:40:12 +0530

    added a input section to get the user to enter the URL link.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {};
   return (
-    <div>Demo</div>
-  )
-}
-export default Demo
+    <section className="mt-16 w-full max-w-xl ">
+      {/* Search */}
+
+      <div className="flex flex-col w-full gap-2">
+        <form
+          action=""
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="text"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit "
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ➤
+          </button>
+        </form>
+
+        {/*Browse URL History */}
+      </div>
+
+      {/*Display Result */}
+    </section>
+  );
+};
+export default Demo;
