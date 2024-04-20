dbd6afadafdbb7e00c2191e4e6786c4950d28761
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-04-20 21:19:01 +0530

    Add the title and the description in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,6 +14,16 @@ const Hero = () => {
           Github
         </button>
       </nav>
+
+      <h1 className="head_text">
+        Summerize Article with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Simplify your reading with Summize , an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
     </header>
   );
 };
